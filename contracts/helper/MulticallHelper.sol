a5b071e8382562216e0f65eb1613f0f2047f71c9
Smea1 <1476977430@qq.com>
2023-02-20 11:28:44 +0800

    modify the helper

--- contracts/helper/MulticallHelper.sol
+++ contracts/helper/MulticallHelper.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.14;
 
 import "../libraries/internals/Utils.sol";
+import "../interfaces/IRouter.sol";
+import "../interfaces/IPriceOracle.sol";
 
 contract MulticallHelper {
     function multicall(address[] memory _targets, bytes[] memory data)
@@ -13,5 +15,26 @@ contract MulticallHelper {
         for (uint256 i = 0; i < _targets.length; i++) {
             results[i] = Utils.lowLevelStaticCall(_targets[i], data[i]);
         }
+    }
+
+    function getTotalDepositedAndBorrowed(IRouter router, IPriceOracle oracle)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        uint256 totalDeposited;
+        uint256 totalBorrowed;
+        uint256 supplyedAmount;
+        uint256 borrowedAmount;
+        uint256 tokenPrice;
+        address[] memory _underlyings = IRouter(router).getUnderlyings();
+        for (uint256 i = 0; i < _underlyings.length; ++i) {
+            supplyedAmount = IRouter(router).totalSupplied(_underlyings[i]);
+            borrowedAmount = IRouter(router).totalBorrowed(_underlyings[i]);
+            tokenPrice = IPriceOracle(oracle).getAssetPrice(_underlyings[i]);
+            totalDeposited += (_supplyedAmount * _tokenPrice) / 1e18;
+            totalBorrowed += (borrowedAmount * _tokenPrice) / 1e18;
+        }
+        return (totalDeposited, totalBorrowed);
     }
 }
