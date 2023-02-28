0ffbec3f8b1a787a4d2f638f48fb358cd3b6653c
cola <49302884+rockmin216@users.noreply.github.com>
2023-02-28 19:27:10 +0800

    fix MockPrice Oracle  value of Asset

--- contracts/mocks/MockPriceOracleOKC.sol
+++ contracts/mocks/MockPriceOracleOKC.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.14;
 
-import "../interfaces/IExOraclePriceData.sol";
-
 contract MockPriceOracleOKC {
     mapping(address => uint256) public units;
     mapping(address => string) public assetSymbol;
@@ -50,7 +48,37 @@ contract MockPriceOracleOKC {
         uint256 priceAsset = getAssetPrice(_asset);
         uint256 priceQuote = getAssetPrice(_quote);
         return
-            (((_amount * priceAsset) / units[_asset]) * units[_quote]) /
-            priceQuote;
+            (_amount * priceAsset * units[_quote]) /
+            (units[_asset] * priceQuote);
     }
+}
+
+interface IExOraclePriceData {
+    function latestRoundData(string calldata priceType, address dataSource)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function get(string calldata priceType, address source)
+        external
+        view
+        returns (uint256 price, uint256 timestamp);
+
+    function getOffchain(string calldata priceType, address source)
+        external
+        view
+        returns (uint256 price, uint256 timestamp);
+
+    function getCumulativePrice(string calldata priceType, address source)
+        external
+        view
+        returns (uint256 cumulativePrice, uint32 timestamp);
+
+    function lastResponseTime(address source) external view returns (uint256);
 }
