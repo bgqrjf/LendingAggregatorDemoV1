873a6e41f1ffbbb7b2be449e67580d2c1c795402
cola <49302884+rockmin216@users.noreply.github.com>
2023-05-25 18:08:04 +0800

    rewrite interface

--- contracts/Compound/CETHInterface.sol
+++ contracts/Compound/CETHInterface.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.18;
 
-import "./CTokenInterface.sol";
+import "./InterestRateModel.sol";
 
-interface CETHInterface is CTokenInterface {
+interface CETHInterface {
     function mint() external payable;
 
     function redeem(uint256 redeemTokens) external returns (uint256);
@@ -11,4 +11,34 @@ interface CETHInterface is CTokenInterface {
     function borrow(uint256 borrowAmount) external returns (uint256);
 
     function repayBorrow() external payable;
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function balanceOfUnderlying(address owner) external view returns (uint256);
+
+    function getAccountSnapshot(
+        address account
+    ) external view returns (uint256, uint256, uint256, uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function borrowBalanceStored(
+        address account
+    ) external view returns (uint256);
+
+    function borrowIndex() external view returns (uint256);
+
+    function interestRateModel() external view returns (InterestRateModel);
+
+    function supplyRatePerBlock() external view returns (uint256);
+
+    function borrowRatePerBlock() external view returns (uint256);
+
+    function reserveFactorMantissa() external view returns (uint256);
+
+    function accrualBlockNumber() external view returns (uint256);
+
+    function totalBorrows() external view returns (uint256);
+
+    function totalReserves() external view returns (uint256);
 }
